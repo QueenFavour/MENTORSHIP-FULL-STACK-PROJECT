b74317f62a8baf5fac1fb22cc8a0753e8405f5d0
Favour Folade <favourfolade@gmail.com>
2025-07-07 16:38:41 +0100

    cleaned again after deployment error

--- app.js
+++ app.js
@@ -54,4 +54,4 @@ app.post('/api/messages', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(Server running on port ${PORT}));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
