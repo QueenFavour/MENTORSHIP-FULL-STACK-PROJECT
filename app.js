c0b9005b85df7e690de8b119e012a9ee8d8bd19f
Favour Folade <favourfolade@gmail.com>
2025-07-07 11:58:15 +0100

    Added test comment

--- app.js
+++ app.js
@@ -36,3 +36,5 @@ app.post('/api/messages', async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
+
+// This is a test change
